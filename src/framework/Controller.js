5d827bdf6f850af9e3485c95de707c786be59971
frank.park <frank.park@kakaocorp.com>
2018-08-05 07:11:41 +0900

    apply Rxjs to Controller

--- src/framework/Controller.js
+++ src/framework/Controller.js
@@ -1,12 +1,20 @@
+import * as Rx from "rxjs";
+
 class Controller {
     constructor(container, template, initStore = {}) {
         this.container = container;
         this.template = template;
-        this.store = initStore;
+        this.state = initStore;
+        this.storeStream = new Rx.Subject();
+        this.storeStream.subscribe(state =>
+            this.container.innerHTML = this.template(state));
+
+        this.storeStream.next(this.state);
     }
 
-    render() {
-        this.container.innerHTML = this.template(this.store);
+    update(newState) {
+        this.state = newState;
+        this.storeStream.next(this.state);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import {getShinhanData, shinhanDataAdaptor} from "./shinhan";
 import RecordTable from "./components/RecordTable";
 import Controller from "./framework/Controller";
 
-const controller = new Controller(document.getElementById("table-wrapper"), RecordTable);
-controller.store = {
-    ...controller.store, ...{
+const controller = new Controller(document.getElementById("table-wrapper"), RecordTable, {
+    records : []
+});
+
+controller.update({
+    ...controller.state, ...{
         records: getShinhanData().map(shinhanDataAdaptor)
     }
-};
-controller.render();
+});
