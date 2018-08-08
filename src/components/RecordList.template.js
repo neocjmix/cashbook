845ab34145adfe42b36a7978977a7fd240317866
frank.park <frank.park@kakaocorp.com>
2018-08-09 01:53:03 +0900

    collapsing day summary. preparing to detach a 'Record' component.

--- src/components/RecordList.template.js
+++ src/components/RecordList.template.js
@@ -6,21 +6,19 @@ export default data => `
     <ul class="day-summary-list">
         ${data.daily.map(day => `
             <li class="day-summary collapsed">
-                <div class="info">
-                    <div class="date-wrap">
-                        <span class="date">${day.date && day.date.format('D')}</span>
-                    </div>
+                <div class="date-wrap">
+                    <span class="date">${day.date && day.date.format('D')}</span>
                 </div>
                 <ul class="record-list">
                     ${day.records.map(record => record.isCreditCard ? `
-                        <li class="record credit-card">
+                        <li class="record record-item credit-card">
                             <dl>
                                 <dt>금액</dt><dd class="amount">${currencyFormatter.format(Math.abs(record.amount), {code: 'KRW'})}</dd>
                                 <dt>내용</dt><dd class="content">${record.content}</dd>
                             </dl>           
                         </li>
                     ` : `
-                        <li class="record bank-account ${record.amount < 0 ? 'withdrawal' : 'deposit'}">
+                        <li class="record record-item bank-account ${record.amount < 0 ? 'withdrawal' : 'deposit'}">
                             <dl>
                                 <dt>금액</dt><dd class="amount">${currencyFormatter.format(Math.abs(record.amount), {code: 'KRW'})}</dd>
                                 <dt>내용</dt><dd class="content">${record.content}</dd>
@@ -28,6 +26,12 @@ export default data => `
                         </li>
                     `).join('\n')}
                 </ul>
+                <div class="record day-summary-content">
+                    <dl>
+                        <dt>금액</dt><dd class="amount">${'...'}</dd>
+                        <dt>내용</dt><dd class="content">합계</dd>
+                    </dl>           
+                </div>
             </li>        
         `).join('\n')}
     <ul class="record-list">
