b11c0537b0433d5e4d2c674f2120a5a26c82a28a
Chanjin Park <frank.park@kakaocorp.com>
2020-02-16 00:13:21 +0900

    change immutable, rxjs -> mobx

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import './base.scss'
-import {getKbCardData, getShinhanBankData, getShinhanCardData} from "./repository";
-import App from "./components/App.template";
-import {bind, Store} from "./Framework";
-import {monthlyAggrigation} from "./reducers";
-import {recentMonths} from "functions"
-import Moment from "moment";
+import {getKbCardData, getShinhanBankData, getShinhanCardData} from './repository'
+import App from './components/App.template'
+import {monthlyAggrigation} from './reducers'
+import {recentMonths} from 'functions'
+import Moment from 'moment'
+import {autorun, configure, observable} from 'mobx'
+
+configure({enforceActions: 'always'});
 
 (async () => {
-    const records = []
-        .concat(await getShinhanBankData())
-        .concat(await getShinhanCardData())
-        .concat(await getKbCardData())
-        .map((record, index) => ({
-            ...record, ...{
-                id: index,
-                dateTime: Moment(record.dateTime)
-            }
-        }))
-        .sort((record1, record2) => record1.dateTime.isBefore(record2.dateTime) ? -1 : 1);
+  const container = document.getElementById('app')
+  const records = []
+    .concat(await getShinhanBankData())
+    .concat(await getShinhanCardData())
+    .concat(await getKbCardData())
+    .map((record, index) => ({
+      ...record, ...{
+        id: index,
+        dateTime: Moment(record.dateTime)
+      }
+    }))
+    .sort((record1, record2) => record1.dateTime.isBefore(record2.dateTime) ? -1 : 1)
 
-    bind(document.getElementById("app"), App, new Store({
-        records : records,
-        monthly: recentMonths(5).map(month => monthlyAggrigation(month, records))
-    }));
-})();
+  const mainStore = observable({
+    records: records,
+    monthly: recentMonths(5).map(month => monthlyAggrigation(month, records))
+  })
+
+  autorun(() => container.innerHTML = App(mainStore))
+})()
